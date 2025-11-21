18d534bc874aa5c9d0a4b3b720e611c7a033f8c8
Terry Henrard <henrardterry2203@hotmail.com>
2025-11-21 18:10:17 +0100

    feat: add FAQ section with accordion functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/features/marketing/components/header";
 import { About } from "@/features/marketing/components/sections/about";
 import { Contact } from "@/features/marketing/components/sections/contact";
+import { Faq } from "@/features/marketing/components/sections/faq";
 import { Hero } from "@/features/marketing/components/sections/hero";
 import { Portfolio } from "@/features/marketing/components/sections/portfolio";
 import { Testimonial } from "@/features/marketing/components/sections/testimonial";
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <Testimonial />
       <Contact />
+      <Faq />
     </main>
   );
 }
