79a588175cd1872004bf6dbaa48f5ee8d01b4ba7
Terry Henrard <henrardterry2203@hotmail.com>
2025-11-24 12:44:38 +0100

    feat: add blog section with dynamic blog posts and blog card component

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,13 @@
+interface BlogPostProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function BlogPost({ params }: BlogPostProps) {
+  const { slug } = await params;
+
+  return (
+    <section className="container m-auto border-b py-24" id={`blog-post-${slug}`}>
+      {slug}
+    </section>
+  );
+}
